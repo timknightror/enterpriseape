da835037898b7b04300efb464e326c035fc766cf
timknightror <tim@52brookedge.com>
2014-10-06 05:49:59 +0000

    nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
+  get 'welcome/faq'
+
+  get 'welcome/pricing'
+
+  get 'welcome/features'
+
   resources :invoices
 
-  root to: 'invoices#index'
+  root to: 'welcome#index'
   
 end
