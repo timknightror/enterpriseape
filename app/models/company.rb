ce5b28923f4bc195874ea88a821d6479e005cafc
timknightror <tim@52brookedge.com>
2015-01-12 11:50:26 +0000

    2015_01_12

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,18 @@
 class Company < ActiveRecord::Base
   
   def self.import(file)
-  CSV.foreach(file.path, headers: true) do |row|
-    Company.create! row.to_hash
+    CSV.foreach(file.path, headers: true) do |row|
+      Company.create! row.to_hash
+    end
   end
+  
+  def self.to_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |company|
+        csv << company.attributes.values_at(*column_names)
+      end
+    end
   end
 end
 
